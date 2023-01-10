86766c9524bd1896278beaeae39f50ba05379b1c
Shushu9 <pokotilo.gleb1@gmail.com>
2023-01-10 13:07:08 +0300

    init: finished this task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minNumber;
+        this.maxNumber;
+        this.guessingNumber;
+    }
 
     setRange(min, max) {
+        this.minNumber = min;
+        this.maxNumber = max;
 
     }
 
     guess() {
-
+        this.guessingNumber = this.minNumber + Math.round((this.maxNumber - this.minNumber) / 2);
+        return this.guessingNumber;
     }
 
     lower() {
+        this.maxNumber = this.guessingNumber;
 
     }
 
     greater() {
-
+        this.minNumber = this.guessingNumber;
     }
 }
 
